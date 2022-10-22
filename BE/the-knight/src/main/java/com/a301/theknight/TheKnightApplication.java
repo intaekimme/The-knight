aefa3266ad362a4ed4b96ad57433395e2ee4a6a1
김현교 <zkdlajt96@naver.com>
2022-10-22 14:14:50 +0900

    Backend first commit

---
+++ BE/the-knight/src/main/java/com/a301/theknight/TheKnightApplication.java
@@ -0,0 +1,13 @@
+package com.a301.theknight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheKnightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheKnightApplication.class, args);
+	}
+
+}
