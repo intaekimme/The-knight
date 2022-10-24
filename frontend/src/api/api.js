997eaf95ab92420799c78d4ab3d5c938ede9d813
박민호 <tkdfhralsgh@naver.com>
2022-10-24 13:28:09 +0900

    feat: setting

---
+++ frontend/src/api/api.js
@@ -0,0 +1,9 @@
+const API_BASE_URL = 'http://localhost:8081/api';
+// const API_BASE_URL = 'https://j7a301.p.ssafy.io/api';
+
+const EXAMPLE = '/example';
+
+const api = {
+  exampleFunction: () => EXAMPLE + `${0}`,
+}
+export default api;
