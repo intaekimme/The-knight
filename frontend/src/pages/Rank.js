4fa9b7f0530fa4af89e1778919f686bf193cb4f9
이현빈 <amy7852@gmail.com>
2022-10-25 17:31:20 +0900

    :lipstick: style : 랭킹페이지 레이아웃 설정

--- frontend/src/pages/Rank.js
+++ frontend/src/pages/Rank.js
@@ -1,7 +1,250 @@
+import { Container } from "@mui/system";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { styled } from '@mui/material/styles';
+import TableCell from '@mui/material/TableCell';
+import Paper from '@mui/material/Paper';
+import { AutoSizer, Column, Table } from 'react-virtualized';
+import 'react-virtualized/styles.css';
+
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import TablePagination from '@mui/material/TablePagination';
+
+const classes = {
+  flexContainer: 'ReactVirtualizedDemo-flexContainer',
+  tableRow: 'ReactVirtualizedDemo-tableRow',
+  tableRowHover: 'ReactVirtualizedDemo-tableRowHover',
+  tableCell: 'ReactVirtualizedDemo-tableCell',
+  noClick: 'ReactVirtualizedDemo-noClick',
+};
+
+const styles = ({ theme }) => ({
+  // temporary right-to-left patch, waiting for
+  // https://github.com/bvaughn/react-virtualized/issues/454
+  '& .ReactVirtualized__Table__headerRow': {
+    ...(theme.direction === 'rtl' && {
+      paddingLeft: '0 !important',
+    }),
+    ...(theme.direction !== 'rtl' && {
+      paddingRight: undefined,
+    }),
+  },
+  [`& .${classes.flexContainer}`]: {
+    display: 'flex',
+    alignItems: 'center',
+    boxSizing: 'border-box',
+  },
+  [`& .${classes.tableRow}`]: {
+    cursor: 'pointer',
+  },
+  [`& .${classes.tableRowHover}`]: {
+    '&:hover': {
+      backgroundColor: theme.palette.grey[200],
+    },
+  },
+  [`& .${classes.tableCell}`]: {
+    flex: 1,
+  },
+  [`& .${classes.noClick}`]: {
+    cursor: 'initial',
+  },
+});
+
+class MuiVirtualizedTable extends React.PureComponent {
+  static defaultProps = {
+    headerHeight: 50,
+    rowHeight: 50,
+  };
+
+  getRowClassName = ({ index }) => {
+    const { onRowClick } = this.props;
+
+    return clsx(classes.tableRow, classes.flexContainer, {
+      [classes.tableRowHover]: index !== -1 && onRowClick != null,
+    });
+  };
+
+  cellRenderer = ({ cellData, columnIndex }) => {
+    const { columns, rowHeight, onRowClick } = this.props;
+    return (
+      <TableCell
+        component="div"
+        className={clsx(classes.tableCell, classes.flexContainer, {
+          [classes.noClick]: onRowClick == null,
+        })}
+        variant="body"
+        style={{ height: rowHeight }}
+        align={
+          (columnIndex != null && columns[columnIndex].numeric) || false
+            ? 'right'
+            : 'left'
+        }
+      >
+        {cellData}
+      </TableCell>
+    );
+  };
+
+  headerRenderer = ({ label, columnIndex }) => {
+    const { headerHeight, columns } = this.props;
+
+    return (
+      <TableCell
+        component="div"
+        className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
+        variant="head"
+        style={{ height: headerHeight }}
+        align={columns[columnIndex].numeric || false ? 'right' : 'left'}
+      >
+        <span>{label}</span>
+      </TableCell>
+    );
+  };
+
+  render() {
+    const { columns, rowHeight, headerHeight, ...tableProps } = this.props;
+    return (
+      <AutoSizer>
+        {({ height, width }) => (
+          <Table
+            height={height}
+            width={width}
+            rowHeight={rowHeight}
+            gridStyle={{
+              direction: 'inherit',
+            }}
+            headerHeight={headerHeight}
+            {...tableProps}
+            rowClassName={this.getRowClassName}
+          >
+            {columns.map(({ dataKey, ...other }, index) => {
+              return (
+                <Column
+                  key={dataKey}
+                  headerRenderer={(headerProps) =>
+                    this.headerRenderer({
+                      ...headerProps,
+                      columnIndex: index,
+                    })
+                  }
+                  className={classes.flexContainer}
+                  cellRenderer={this.cellRenderer}
+                  dataKey={dataKey}
+                  {...other}
+                />
+              );
+            })}
+          </Table>
+        )}
+      </AutoSizer>
+    );
+  }
+}
+
+MuiVirtualizedTable.propTypes = {
+  columns: PropTypes.arrayOf(
+    PropTypes.shape({
+      dataKey: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      numeric: PropTypes.bool,
+      width: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+  headerHeight: PropTypes.number,
+  onRowClick: PropTypes.func,
+  rowHeight: PropTypes.number,
+};
+
+const VirtualizedTable = styled(MuiVirtualizedTable)(styles);
+
+// ---
+
+const sample = [
+  ['Frozen yoghurt', 159],
+  ['Ice cream sandwich', 237],
+  ['Eclair', 262],
+  ['Cupcake', 305],
+  ['Gingerbread', 356],
+];
+
+function createData(id, name, score) {
+  id = id + 1;
+  return { id, name, score };
+}
+
+const rows = [];
+
+for (let i = 0; i < 200; i += 1) {
+  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
+  rows.push(createData(i, ...randomSelection));
+}
+
 export default function Rank() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(20);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+
   return (
-    <div>
-      랭킹
-    </div>
+    <Container>
+      <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 500 }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Name"
+        inputProps={{ 'aria-label': 'search name' }}
+      />
+      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+      </Paper>
+      <br/>
+      <Paper style={{ height: 500, width: '100%' }}>
+      <VirtualizedTable
+        rowCount={rows.length}
+        rowGetter={({ index }) => rows[index]}
+        columns={[
+          {
+            width: 300,
+            label: '랭킹',
+            dataKey: 'id',
+          },
+          {
+            width: 500,
+            label: '닉네임',
+            dataKey: 'name',
+            numeric: true,
+          },
+          {
+            width: 300,
+            label: '점수',
+            dataKey: 'score',
+            numeric: true,
+          },
+        ]}
+        />
+        <TablePagination
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+      </Paper>
+    </Container>
   );
 }
