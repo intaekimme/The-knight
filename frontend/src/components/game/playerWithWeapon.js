db6004d9a8d9c2f62da92848d63b7136dfd9a4f0
이현진 <hjnee1102@gmail.com>
2022-10-25 14:28:59 +0900

    :lipstick: style : 코드 포매팅

--- frontend/src/components/game/playerWithWeapon.js
+++ frontend/src/components/game/playerWithWeapon.js
@@ -1,14 +1,17 @@
-import Player from './player';
-import Grid from '@mui/material/Grid';
+import Player from "./player";
+import Grid from "@mui/material/Grid";
 
 function PlayerWithWeapon({ isMe, nickName }) {
   return (
     <Grid container>
       <div style={{ width: 50, height: 50, backgroundColor: "grey" }}></div>
-      <Player isMe={isMe} nickName={ nickName } />
+      <Player
+        isMe={isMe}
+        nickName={nickName}
+      />
       <div style={{ width: 50, height: 50, backgroundColor: "grey" }}></div>
     </Grid>
-  )
+  );
 }
 
-export default PlayerWithWeapon
+export default PlayerWithWeapon;
