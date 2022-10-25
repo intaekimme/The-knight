ecc762250a248a26cc4400bbdd49b8388d159de5
LeeHyunJin1997 <hjnee1102s@gmail.com>
2022-10-25 21:56:08 +0900

    :bug: fix : slice 파일명 에러 수정
    - 이전 커밋에서 LoginSlice가 loginSlice로 변경되었음
    - 이를 참조하는 store.js, LoginToken.js에서 이름을 변경하지 않아 오류
    - 정정

--- frontend/src/_store/store.js
+++ frontend/src/_store/store.js
@@ -1,10 +1,10 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import loginReducer from '../_slice/LoginSlice';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import loginReducer from "../_slice/loginSlice";
 
 const store = configureStore({
   reducer: {
     login: loginReducer,
-  }
+  },
 });
 
 export default store;
