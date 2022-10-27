21a5bbbb55350b4660c0c29ad3f3fbc926ce5dcd
InTae <kit938639@gmail.com>
2022-10-27 23:34:18 +0900

    :sparkles: feat 게임 대기 방 예외처리 - 커스텀 예외처리로 변경

---
+++ BE/the-knight/src/main/java/com/a301/theknight/global/error/errorcode/GameWaitingErrorCode.java
@@ -0,0 +1,19 @@
+package com.a301.theknight.global.error.errorcode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum GameWaitingErrorCode implements ErrorCode {
+
+    GAME_IS_NOT_READY_STATUS(HttpStatus.BAD_REQUEST, "The game room status is not in waiting status."),
+    NO_PERMISSION_TO_DELETE_GAME_ROOM(HttpStatus.BAD_REQUEST, "Only the owner can delete a room."),
+    NO_PERMISSION_TO_MODIFY_GAME_ROOM(HttpStatus.BAD_REQUEST, "Only the owner can modify a room.")
+
+
+    ;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
