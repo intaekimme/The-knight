87ffdd375018a349112170207438c2e2df0ce937
이현빈 <amy7852@gmail.com>
2022-10-28 17:47:37 +0900

    :lemon: modify : 안쓰는 reducer 제거

--- frontend/src/_store/store.js
+++ frontend/src/_store/store.js
@@ -1,7 +1,6 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import loginReducer from "../_slice/loginSlice";
 
-import playersReducer from "../_slice/playersSlice";
 import memberInfoReducer from "../_slice/memberInfoSlice";
 import gameReducer from "../_slice/gameSlice";
 
@@ -11,7 +10,6 @@ import gameReducer from "../_slice/gameSlice";
 const store = configureStore({
   reducer: {
     login: loginReducer,
-    players: playersReducer,
     memberInfo: memberInfoReducer,
     game: gameReducer,
   },
