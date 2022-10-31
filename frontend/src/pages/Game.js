93b809da29e213dbed631a85a3a5b0a8fe940727
이현진 <hjnee1102@gmail.com>
2022-10-31 17:50:21 +0900

    :sparkles: feat : Fade-in 기능 생성

--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { useSpring, animated } from "react-spring";
 
 import LoginCheck from "../commons/login/LoginCheck";
 import Phase0 from "../components/game/Phase0";
@@ -16,10 +17,12 @@ export default function Game() {
   //   }
   // }, []);
 
+  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 }, config: { duration: 1500 } })
   const phase = useSelector(state => state.game.phase)
 
   return (
     <div>
+      <animated.div style={props}>I will fade in</animated.div>
       {phase === 0 ? <Phase0></Phase0> : null}
       {phase === 1 ? <Phase1></Phase1> : null}
     </div>
