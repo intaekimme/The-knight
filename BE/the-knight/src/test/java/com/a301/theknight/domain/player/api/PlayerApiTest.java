3af8aee9f39ef30d12abfe64bafd6252186f5058
intae <kit938639@gmail.com>
2022-10-31 18:00:50 +0900

    :sparkles: feat 플레이어 웹소켓 api 서비스 테스트 - 종단 간 테스트가 오류로 안되서 서비스 테스로 변경 - Owner ready test만 작성하면 됨. - 테스트 후 엔티티 원복

---
+++ BE/the-knight/src/test/java/com/a301/theknight/domain/player/api/PlayerApiTest.java
@@ -0,0 +1,141 @@
+package com.a301.theknight.domain.player.api;
+
+import com.a301.theknight.domain.game.entity.Game;
+import com.a301.theknight.domain.game.repository.GameRepository;
+import com.a301.theknight.domain.member.entity.Member;
+import com.a301.theknight.domain.member.repository.MemberRepository;
+import com.a301.theknight.domain.player.dto.*;
+import com.a301.theknight.domain.player.entity.Player;
+import com.a301.theknight.domain.player.entity.Team;
+import com.a301.theknight.domain.player.repository.PlayerRepository;
+import com.a301.theknight.domain.player.service.PlayerWebsocketService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class PlayerApiTest {
+    @Mock
+    MemberRepository memberRepository;
+
+    @Mock
+    GameRepository gameRepository;
+
+    @Mock
+    PlayerRepository playerRepository;
+
+    PlayerWebsocketService playerWebsocketService;
+
+    static Member[] testMembers;
+    static Game testGame;
+
+    static Player[] testPlayers;
+
+    static PlayerTeamRequest playerTeamRequest;
+    static PlayerReadyRequest playerReadyRequest;
+
+    @BeforeAll
+    static void beforeAll() {
+        testMembers = new Member[11];
+
+        for(int i=1; i<=10; i++){
+            testMembers[i] = new Member(i,
+                    "testPlayer" + Integer.toString(i) + "@email.com", "testPlayer" + Integer.toString(i),
+                    "testPlayer" + Integer.toString(i),
+                    "testPlayer" + Integer.toString(i),
+                    "testImage" + Integer.toString(i));
+        }
+
+        testGame = new Game(1L, "testGame", 4,3,2,1,10 );
+
+        testPlayers = new Player[11];
+        for(int i = 1; i < testMembers.length-1; i++){
+            testPlayers[i] = new Player(i, testMembers[i], testGame);
+            if(i % 2 == 0){
+                testPlayers[i].selectTeam(Team.B);
+            }
+        }
+
+        playerTeamRequest  = new PlayerTeamRequest();
+        playerTeamRequest.setTeam("B");
+
+        playerReadyRequest = new PlayerReadyRequest();
+        playerReadyRequest.setReadyStatus(true);
+
+    }
+
+    @BeforeEach
+    void setup() { playerWebsocketService = new PlayerWebsocketService(memberRepository, gameRepository, playerRepository); }
+
+    @Test
+    @DisplayName("Player entry test")
+    void PlayerEntryResponse() {
+        given(memberRepository.findById(10L)).willReturn(Optional.of(testMembers[10]));
+        given(gameRepository.findById(1L)).willReturn(Optional.of(testGame));
+
+        PlayerEntryResponse playerEntryResponse = playerWebsocketService.entry(1L, 10L);
+
+    }
+
+    @Test
+    @DisplayName("Player exit test")
+    void long_exitPlayerId() {
+        given(memberRepository.findById(1L)).willReturn(Optional.of(testMembers[1]));
+        given(gameRepository.findById(1L)).willReturn(Optional.of(testGame));
+        given(playerRepository.findByGameAndMember(testGame, testMembers[1])).willReturn(Optional.of(testPlayers[1]));
+
+        long exitPlayerId = playerWebsocketService.exit(1L, 1L);
+        assertEquals(1L, exitPlayerId);
+    }
+
+    @Test
+    @DisplayName("Team change test")
+    void playerTeamResponse() {
+        given(memberRepository.findById(1L)).willReturn(Optional.of(testMembers[1]));
+        given(gameRepository.findById(1L)).willReturn(Optional.of(testGame));
+        given(playerRepository.findByGameAndMember(testGame, testMembers[1])).willReturn(Optional.of(testPlayers[1]));
+
+        PlayerTeamResponse playerTeamResponse = playerWebsocketService.team(1L, 1L, playerTeamRequest);
+
+        assertEquals(playerTeamRequest.getTeam(), playerTeamResponse.getTeam());
+        assertEquals(1L, playerTeamResponse.getPlayerId());
+    }
+
+    @Test
+    @DisplayName("Player ready test")
+    void normalPlayerRequest() {
+        given(memberRepository.findById(2L)).willReturn(Optional.of(testMembers[2]));
+        given(gameRepository.findById(1L)).willReturn(Optional.of(testGame));
+        given(playerRepository.findByGameAndMember(testGame, testMembers[2])).willReturn(Optional.of(testPlayers[2]));
+
+        ReadyResponseDto readyResponseDto = playerWebsocketService.ready(1L, 2L, playerReadyRequest);
+        PlayerReadyResponse playerReadyResponse = readyResponseDto.getPlayerReadyResponseList().get(0);
+
+        assertEquals(2L, playerReadyResponse.getPlayerId());
+        assertTrue(playerReadyResponse.isReadyStatus());
+        assertFalse(playerReadyResponse.isStartFlag());
+
+    }
+
+    @Test
+    @DisplayName("Owner ready test")
+    void ownerPlayerRequest() {
+        given(memberRepository.findById(1L)).willReturn(Optional.of(testMembers[1]));
+        given(gameRepository.findById(1L)).willReturn(Optional.of(testGame));
+        given(playerRepository.findByGameAndMember(testGame, testMembers[1])).willReturn(Optional.of(testPlayers[1]));
+
+        ReadyResponseDto readyResponseDto = playerWebsocketService.ready(1L, 1L, playerReadyRequest);
+
+        assertTrue(readyResponseDto.isOwner());
+    }
+
+}
