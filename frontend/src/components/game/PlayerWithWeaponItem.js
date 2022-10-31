45a47ade871b2d5c7bbc55f8bd1925a30dd962e5
이현진 <hjnee1102@gmail.com>
2022-10-31 16:25:17 +0900

    :sparkles: feat : Game 페이지 phase 분할
    - 게임 중 phase에 따라 화면이 전환되도록 함

--- frontend/src/components/game/PlayerWithWeaponItem.js
+++ frontend/src/components/game/PlayerWithWeaponItem.js
@@ -1,11 +1,12 @@
 import Player from "./Player";
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { deleteWeapon } from "../../_slice/gameSlice"
 import Grid from "@mui/material/Grid";
 import CancelIcon from '@mui/icons-material/Cancel';
 
 // 추후에 isMe 삭제
 function PlayerWithWeaponItem({ isMe, userName, weapons, isOpp }) {
+  const phase = useSelector(state => state.game.phase)  
   const dispatch = useDispatch();
   function deleteLeft() {
     dispatch(deleteWeapon('left'))
@@ -15,20 +16,24 @@ function PlayerWithWeaponItem({ isMe, userName, weapons, isOpp }) {
   }
 
   return (
-    <Grid container>
-      <div style={{ width: 50, height: 50, backgroundColor: "grey" }}>
-        {weapons[0] && <CancelIcon onClick={deleteLeft} style={isOpp ? {display: "none"} : null}></CancelIcon>}
-        <span style={isOpp ? {display: "none"} : null}>{weapons[0]}</span>
-      </div>
-      <Player
-        isMe={isMe}
-        userName={userName}
-      />
-      <div style={{ width: 50, height: 50, backgroundColor: "grey" }}>
-        {weapons[1] && <CancelIcon onClick={deleteRight} style={isOpp ? {display: "none"} : null}></CancelIcon>}
-        <span style={isOpp ? {display: "none"} : null}>{weapons[1]}</span>
-      </div>
-    </Grid>
+    <div>
+      <Grid container>
+        <div style={{ width: 50, height: 50, backgroundColor: "grey" }}>
+          {weapons[0] && <CancelIcon onClick={deleteLeft} style={isOpp ? {display: "none"} : null}></CancelIcon>}
+          <span style={isOpp ? {display: "none"} : null}>{weapons[0]}</span>
+        </div>
+        <Player
+          isMe={isMe}
+          userName={userName}
+        />
+        <div style={{ width: 50, height: 50, backgroundColor: "grey" }}>
+          {weapons[1] && <CancelIcon onClick={deleteRight} style={isOpp ? {display: "none"} : null}></CancelIcon>}
+          <span style={isOpp ? {display: "none"} : null}>{weapons[1]}</span>
+        </div>
+      </Grid>
+      {/* 순서 표시하는 로직 필요 */}
+      {(phase=== 0) ? null : <div style={{ width: 50, height: 50, border: "solid" }}></div>}
+    </div>
   );
 }
 
