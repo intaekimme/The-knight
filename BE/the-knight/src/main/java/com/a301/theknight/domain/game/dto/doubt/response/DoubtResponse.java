a5d44329933ccf3e26aa252e6769368df89bd17b
김현교 <zkdlajt96@naver.com>
2022-11-03 15:04:47 +0900

    :lipstick: style : 주석 삭제

--- BE/the-knight/src/main/java/com/a301/theknight/domain/game/dto/doubt/response/DoubtResponse.java
+++ BE/the-knight/src/main/java/com/a301/theknight/domain/game/dto/doubt/response/DoubtResponse.java
@@ -15,32 +15,3 @@ public class DoubtResponse {
     private DoubtPlayerIdDto nextDefender;
     private boolean SuspiciousResult;
 }
-
-/*
-    * 의심
-{
-  suspect : {
-    id : long : 의심하는 사람 Id,
-    isDead : boolean : 사망 여부,
-  }
-  suspected : {
-    id : long : 의심 당하는 사람
-    isDead : boolean : 사망 여부,
-  }
-  open : {
-    memberId : long,
-    weapon : String
-    (SWORD, TWIN, SHEILD, HAND, HIDE)
-    hand : String (LEFT, RIGHT)
-  }
-  isNextDefend : boolean : 이후 방어 수행여부
-  nextDefender : {
-    id : long : 다음 방어자 id
-  }
-  isSuspectSuccess : boolean : 의심 성공 여부
-  }
-  *
-  * }
-}
-    *
-    * */
