610ff18fd01f35091d6c67a9c53c644f4ab36fef
박민호 <tkdfhralsgh@naver.com>
2022-11-03 14:10:14 +0900

    modify: goolge Login 수정, enterRoom websocket 연결 수정중

--- frontend/src/functions/lobby/EnterRoom.js
+++ frontend/src/functions/lobby/EnterRoom.js
@@ -6,18 +6,24 @@ import api from '../../api/api';
 import {setRoom, setUsers} from '../../_slice/roomSlice';
 
 export default function EnterRoom(gameId){
-  const [userData, setUserData] = useState({
-    sender: "",
-    receiver: "ALL", // ALL / A / B
-    connected: false,
-    message: "",
-  });
+  // const [members, setMembers] = useState({
+  //   sender: "",
+  //   receiver: "ALL", // ALL / A / B
+  //   connected: false,
+  //   message: "",
+  // });
+  const [connected, setConnected] = useState(false);
+  const [members, setMembers] = useState([{
+      id : -1,
+      nickname : "닉네임",
+      team: "A", // A / B
+    },
+  ]);
 
   const connect = () => {
     // config/WebsocketConfig.java registerStompEndpoints 를 websocket/pub/sub로 설정했기 때문에 마지막에 websocket/pub/sub 있어야함
     console.log(window.localStorage.getItem("loginToken"));
-    // let Sock = new SockJS(`https://j7a301.p.ssafy.io/websocket?token=${window.localStorage.getItem("loginToken")}`);
-    let Sock = new SockJS(`http://localhost:8080/websocket?token=${window.localStorage.getItem("loginToken")}`);
+    let Sock = new SockJS(`${api.websocket()}?token=${window.localStorage.getItem("loginToken")}`);
     stompClient = over(Sock);
     stompClient.connect({Authorization: `Bearer ${window.localStorage.getItem("loginToken")}`}, onConnected, (error) => {
       console.log(error);
@@ -25,7 +31,8 @@ export default function EnterRoom(gameId){
   };
 
   const onConnected = () => {
-    setUserData({ ...userData, connected: true });
+    setConnected(true);
+    // setUserData({ ...userData, connected: true });
     stompClient.subscribe(api.enterRoom(gameId), onMessageReceived, (error) => {
       console.log(error);
     });
