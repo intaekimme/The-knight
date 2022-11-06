5a2ce21cf78c8b0ea14f15b8bdb5bdc0d0a0a8b3
김현교 <zkdlajt96@naver.com>
2022-11-07 00:09:13 +0900

    :sparkles: feat : 랭킹리스트 쿼리 작성
    - projection을 이용한 DTO 응답 매핑.
    - 추후 테스트 필요.

--- BE/the-knight/src/main/java/com/a301/theknight/domain/ranking/repository/RankingRepository.java
+++ BE/the-knight/src/main/java/com/a301/theknight/domain/ranking/repository/RankingRepository.java
@@ -1,10 +1,14 @@
 package com.a301.theknight.domain.ranking.repository;
 
+import com.a301.theknight.domain.ranking.dto.RankingDto;
 import com.a301.theknight.domain.ranking.entity.Ranking;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 
 public interface RankingRepository extends JpaRepository<Ranking, Long> {
@@ -15,4 +19,26 @@ public interface RankingRepository extends JpaRepository<Ranking, Long> {
             "FROM ranking) r " +
             "WHERE member_id = :memberId", nativeQuery = true)
     int findMemberRanking(@Param("memberId") long memberId);
+
+    @Query(value = "SELECT m.nickname, m.image, rank() OVER (ORDER BY r.score DESC) AS ranking, r.score " +
+            "FROM ranking AS r LEFT JOIN member AS m " +
+            "ON r.member_id = m.id " +
+            "ORDER BY ranking ASC",
+            countQuery = "SELECT count(*) " +
+                    "FROM ranking AS r LEFT JOIN member AS m " +
+                    "ON r.member_id = m.id ",
+            nativeQuery = true)
+    Page<RankingDto> getRankingList(Pageable pageable);
+
+    @Query(value = "SELECT m.nickname, m.image, rank() OVER (ORDER BY r.score DESC) AS ranking, r.score " +
+            "FROM ranking AS r LEFT JOIN member AS m " +
+            "ON r.member_id = m.id " +
+            "WHERE m.nickname LIKE %:keyword% " +
+            "ORDER BY ranking ASC",
+            countQuery = "SELECT count(*) " +
+                    "FROM ranking AS r LEFT JOIN member AS m " +
+                    "ON r.member_id = m.id " +
+                    "WHERE m.nickname LIKE %:keyword%",
+            nativeQuery = true)
+    Page<RankingDto> getRankingListByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
