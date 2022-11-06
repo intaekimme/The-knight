21c203e323f800ad35fe40f81964b0e4a0e613e6
김현교 <zkdlajt96@naver.com>
2022-11-06 18:18:35 +0900

    :sparkles: feat : 화면 전환 대답에 동시성 문제 처리
    - 여러 클라이언트의 동시적인 요청을 처리하도록 Redisson 라이브러리의 Rlock 활용.

--- BE/the-knight/src/main/java/com/a301/theknight/domain/game/service/GameConvertService.java
+++ BE/the-knight/src/main/java/com/a301/theknight/domain/game/service/GameConvertService.java
@@ -1,18 +1,20 @@
 package com.a301.theknight.domain.game.service;
 
 import com.a301.theknight.domain.game.dto.convert.GameStatusResponse;
-import com.a301.theknight.domain.game.entity.GameStatus;
 import com.a301.theknight.domain.game.entity.redis.InGame;
 import com.a301.theknight.domain.game.entity.redis.InGamePlayer;
-import com.a301.theknight.domain.game.entity.redis.TurnStatus;
 import com.a301.theknight.domain.game.repository.GameRedisRepository;
 import com.a301.theknight.domain.game.util.GameConvertUtil;
+import com.a301.theknight.global.error.errorcode.DomainErrorCode;
 import com.a301.theknight.global.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static com.a301.theknight.global.error.errorcode.GamePlayingErrorCode.INGAME_IS_NOT_EXIST;
 import static com.a301.theknight.global.error.errorcode.GamePlayingErrorCode.INGAME_PLAYER_IS_NOT_EXIST;
@@ -22,17 +24,28 @@ import static com.a301.theknight.global.error.errorcode.GamePlayingErrorCode.ING
 public class GameConvertService {
     private final GameRedisRepository gameRedisRepository;
     private final GameConvertUtil gameConvertUtil;
+    private final RedissonClient redissonClient;
 
     @Transactional
     public List<String> convertComplete(long gameId, long memberId) {
-        InGame inGame = getInGame(gameId);
+        RLock lock = redissonClient.getLock(lockKeyGen(gameId));
+        try {
+            boolean available = lock.tryLock(5, 2, TimeUnit.SECONDS);
+            if (!available) {
+                throw new CustomException(DomainErrorCode.FAIL_TO_ACQUIRE_REDISSON_LOCK);
+            }
 
-        inGame.addRequestCount();
-        gameRedisRepository.saveInGame(gameId, inGame);
-        if (!inGame.isFullCount()) {
-            return null;
+            InGame inGame = getInGame(gameId);
+            inGame.addRequestCount();
+            gameRedisRepository.saveInGame(gameId, inGame);
+
+            return inGame.isFullCount() ?
+                    gameConvertUtil.getPostfixList(inGame.getGameStatus().name()) : null;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
         }
-        return gameConvertUtil.getPostfixList(inGame.getGameStatus().name());
     }
 
     @Transactional
@@ -44,8 +57,6 @@ public class GameConvertService {
         return new GameStatusResponse(inGame.getGameStatus().name());
     }
 
-
-
     private InGame getInGame(long gameId) {
         return gameRedisRepository.getInGame(gameId)
                 .orElseThrow(() -> new CustomException(INGAME_IS_NOT_EXIST));
@@ -54,6 +65,10 @@ public class GameConvertService {
     private InGamePlayer getInGamePlayer(long gameId, long memberId) {
         return gameRedisRepository.getInGamePlayer(gameId, memberId)
                 .orElseThrow(() -> new CustomException(INGAME_PLAYER_IS_NOT_EXIST));
+    }
+
+    private String lockKeyGen(long gameId) {
+        return "game:" + gameId + "_convert_lock";
     }
 
 }
