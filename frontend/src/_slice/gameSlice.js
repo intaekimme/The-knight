38c628065165df34e4bd3e6804ecc5f4484e2b65
LeeHyunJin1997 <hjnee1102s@gmail.com>
2022-11-06 16:49:15 +0900

    :sparkles: feat : select weapon & order with me

--- frontend/src/_slice/gameSlice.js
+++ frontend/src/_slice/gameSlice.js
@@ -69,8 +69,8 @@ const playersInit = {
 }
 
 const orderInit = {
-  teamA: [null, null, null, null, null],
-  teamB: [null, null, null, null, null],
+  A: [null, null, null, null, null],
+  B: [null, null, null, null, null],
 }
 
 // PRE, ATTACK, DEFEND, DOUBT, SHIELD, RESULT, END
@@ -104,38 +104,34 @@ export const gameSlice = createSlice({
       state.players = action.payload
     },
     selectWeapon: (state, action) => {
-      // 지금은 첫 플레이어의 무기를 바꾸는 것으로 고정
-      // 추후에 해당 유저의 무기를 바꾸는 것으로 변경
-      if (!state.players.players[0].weapons[0]) {
-        state.players.players[0].weapons[0] = action.payload
-      } else if (!state.players.players[0].weapons[1]) {
-        state.players.players[0].weapons[1] = action.payload
+      if (!state.players.players[state.me.index].weapons[0]) {
+        state.players.players[state.me.index].weapons[0] = action.payload
+      } else if (!state.players.players[state.me.index].weapons[1]) {
+        state.players.players[state.me.index].weapons[1] = action.payload
       }
     },
     deleteWeapon: (state, action) => {
       if (action.payload === 'left') {
-        state.players.players[0].weapons[0] = null
+        state.players.players[state.me.index].weapons[0] = null
       } else if (action.payload === 'right') {
-        state.players.players[0].weapons[1] = null
+        state.players.players[state.me.index].weapons[1] = null
       }
     },
     selectOrder: (state, action) => {
-      // 지금은 첫 플레이어의 무기를 바꾸는 것으로 고정
-      // 추후에 해당 유저의 무기를 바꾸는 것으로 변경
       // 선택한 순서가 비어있고, 유저가 이미 선택한 순서가 없을 때
-      if (!state.order.teamA[action.payload]) {
-        for (let i=0; i < state.order.teamA.length; i++) {
+      if (!state.order[state.me.team][action.payload]) {
+        for (let i=0; i < state.order[state.me.team].length; i++) {
           // 선택한 순서가 비어있고, (다른 순서를 이미 선택해뒀을 때)
-          if (state.order.teamA[i]) {
-            if (state.order.teamA[i].memberId === state.players.players[0].memberId) {
-              state.order.teamA[i] = null
+          if (state.order[state.me.team][i]) {
+            if (state.order[state.me.team][i].memberId === state.players.players[state.me.index].memberId) {
+              state.order[state.me.team][i] = null
             }
           }
         }
-        state.order.teamA[action.payload] = state.players.players[0]
+        state.order[state.me.team][action.payload] = state.players.players[state.me.index]
         // (선택한 순서가 차있고), 유저가 선택했던 순서일 때
-      } else if (state.order.teamA[action.payload].memberId === state.players.players[0].memberId) {
-        state.order.teamA[action.payload] = null
+      } else if (state.order[state.me.team][action.payload].memberId === state.players.players[state.me.index].memberId) {
+        state.order[state.me.team][action.payload] = null
       }
     },
     fetchPhase: (state, action) => {
