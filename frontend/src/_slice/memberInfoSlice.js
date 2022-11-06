996e3f1d26eb50ed007d459a36d2700bd6170acb
Hyunbin-Lee <amy7852@gmail.com>
2022-11-07 08:35:33 +0900

    add member info api

--- frontend/src/_slice/memberInfoSlice.js
+++ frontend/src/_slice/memberInfoSlice.js
@@ -1,26 +1,28 @@
 import React from "react";
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import api from "../api/api";
 
 const memberInfoSliceInit = {
-    confirmPassword: false,
-    MemberInfo: {
-      id: 1,
-      nickname: '더나이트최강자',
-      image: 'https://picsum.photos/id/237/200/300',
-      ranking: 1,
-      score: 323,
-      win: 7,
-      lose: 3,
-      currentReco: [
-        {
-          gameId: 1,
-          result: '승',
-          capacity: 10,
-          sword: 2,
-          twin: 2,
-          shield: 2,
-          hand: 2,
-          alliance: [
+  confirmPassword: false,
+  MemberInfo: {
+    id: 1,
+    nickname: '더나이트최강자',
+    image: 'https://picsum.photos/id/237/200/300',
+    ranking: 1,
+    score: 323,
+    win: 7,
+    lose: 3,
+    currentReco: [
+      {
+        gameId: 1,
+        result: '승',
+        capacity: 10,
+        sword: 2,
+        twin: 2,
+        shield: 2,
+        hand: 2,
+        alliance: [
           {
             image: '',
             nickname: '우리팀임여1',
@@ -74,59 +76,59 @@ const memberInfoSliceInit = {
         shield: 3,
         hand: 1,
         alliance: [
-        {
-          image: '',
-          nickname: 'ㅋ우리팀임여1',
-        },
-        {
-          image: '',
-          nickname: 'ㅋ우리팀임여2',
-        },
-        {
-          image: '',
-          nickname: 'ㅋ우리팀임여3',
-        },
-        {
-          image: '',
-          nickname: 'ㅋ우리팀임여4',
-        },
-        {
-          image: '',
-          nickname: '더나이트최강자',
-        },
-      ],
-      opposite: [
-        {
-          image: '',
-          nickname: '적팀임여1',
-        },
-        {
-          image: '',
-          nickname: '적팀임여2',
-        },
-        {
-          image: '',
-          nickname: '적팀임여3',
-        },
-        {
-          image: '',
-          nickname: '적팀임여4',
-        },
-        {
-          image: '',
-          nickname: '적팀임여5',
-        },
-      ],
+          {
+            image: '',
+            nickname: 'ㅋ우리팀임여1',
+          },
+          {
+            image: '',
+            nickname: 'ㅋ우리팀임여2',
+          },
+          {
+            image: '',
+            nickname: 'ㅋ우리팀임여3',
+          },
+          {
+            image: '',
+            nickname: 'ㅋ우리팀임여4',
+          },
+          {
+            image: '',
+            nickname: '더나이트최강자',
+          },
+        ],
+        opposite: [
+          {
+            image: '',
+            nickname: '적팀임여1',
+          },
+          {
+            image: '',
+            nickname: '적팀임여2',
+          },
+          {
+            image: '',
+            nickname: '적팀임여3',
+          },
+          {
+            image: '',
+            nickname: '적팀임여4',
+          },
+          {
+            image: '',
+            nickname: '적팀임여5',
+          },
+        ],
       },
-        {
-          gameId: 1,
-          result: '승',
-          capacity: 10,
-          sword: 2,
-          twin: 2,
-          shield: 2,
-          hand: 2,
-          alliance: [
+      {
+        gameId: 1,
+        result: '승',
+        capacity: 10,
+        sword: 2,
+        twin: 2,
+        shield: 2,
+        hand: 2,
+        alliance: [
           {
             image: '',
             nickname: '우리팀임여1',
@@ -180,52 +182,73 @@ const memberInfoSliceInit = {
         shield: 3,
         hand: 1,
         alliance: [
-        {
-          image: '',
-          nickname: 'ㅋ우리팀임여1',
-        },
-        {
-          image: '',
-          nickname: 'ㅋ우리팀임여2',
-        },
-        {
-          image: '',
-          nickname: 'ㅋ우리팀임여3',
-        },
-        {
-          image: '',
-          nickname: 'ㅋ우리팀임여4',
-        },
-        {
-          image: '',
-          nickname: '더나이트최강자',
-        },
-      ],
-      opposite: [
-        {
-          image: '',
-          nickname: '적팀임여1',
-        },
-        {
-          image: '',
-          nickname: '적팀임여2',
-        },
-        {
-          image: '',
-          nickname: '적팀임여3',
-        },
-        {
-          image: '',
-          nickname: '적팀임여4',
-        },
-        {
-          image: '',
-          nickname: '적팀임여5',
-        },
-      ],
+          {
+            image: '',
+            nickname: 'ㅋ우리팀임여1',
+          },
+          {
+            image: '',
+            nickname: 'ㅋ우리팀임여2',
+          },
+          {
+            image: '',
+            nickname: 'ㅋ우리팀임여3',
+          },
+          {
+            image: '',
+            nickname: 'ㅋ우리팀임여4',
+          },
+          {
+            image: '',
+            nickname: '더나이트최강자',
+          },
+        ],
+        opposite: [
+          {
+            image: '',
+            nickname: '적팀임여1',
+          },
+          {
+            image: '',
+            nickname: '적팀임여2',
+          },
+          {
+            image: '',
+            nickname: '적팀임여3',
+          },
+          {
+            image: '',
+            nickname: '적팀임여4',
+          },
+          {
+            image: '',
+            nickname: '적팀임여5',
+          },
+        ],
       },
     ]
-    },
+  },
+}
+
+const fetchMemberInfo = createAsyncThunk('fetchMemberInfo', async () => {
+  try {
+    const res = await axios.get(api.getMemberInfo(), {
+      headers: { Authorization: `Bearer ${window.localStorage.getItem("loginToken")}` }
+    });
+    console.log("member info", res.data);
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+const memberInfoInit = {
+  nickname: '',
+  image: '',
+  ranking: -1,
+  score: -1,
+  win: -1,
+  lose: -1,
 }
 
 export const memberInfoSlice = createSlice({
@@ -234,8 +257,18 @@ export const memberInfoSlice = createSlice({
   reducers: {
     changeImage: (state, action) => {
       state.value.MemberInfo.image = action.payload
+    },
   },
-  },
+  extraReducers: {
+    [fetchMemberInfo.fulfilled]: (state, action) => {
+      state.fetchMemberInfo = action.payload;
+    },
+    [fetchMemberInfo.rejected]: state => {
+      state.fetchMemberInfo = memberInfoInit;
+    },
+  }
 });
+
+export { fetchMemberInfo };
 export const { changeImage } = memberInfoSlice.actions;
 export default memberInfoSlice.reducer;
--- frontend/src/components/memberPage/MemberInfoForm.js
+++ frontend/src/components/memberPage/MemberInfoForm.js
@@ -1,32 +1,42 @@
 import React from "react";
-import {useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { Avatar, Button, Grid, Paper } from "@mui/material";
+import { fetchMemberInfo } from "../../_slice/memberInfoSlice";
 
-export default function MemberInfoForm({updateProfile}) {
+export default function MemberInfoForm({ updateProfile }) {
+  const [isSetting, setIsSetting] = React.useState(false);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    if (!isSetting) {
+      setIsSetting(true);
+      dispatch(fetchMemberInfo());
+    }
+  }, []);
+
   const memberInfo = useSelector(state => state.memberInfo.value.MemberInfo);
   console.log("memberInfo", memberInfo);
   return (
-      <Grid container sx={{pt:5}} rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }} justifyContent="center" alignItems="center">
-        <Grid item xs={3} >
-            <Avatar
-              alt="Remy Sharp"
-              src="/static/images/avatar/1.jpg"
-              sx={{ width: 250, height: 250 }}
-            >
-            </Avatar>
-        </Grid>
-        <Grid item xs={7}>
-        <Paper sx={{boxShadow: "none"}}>
+    <Grid container sx={{ pt: 5 }} rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }} justifyContent="center" alignItems="center">
+      <Grid item xs={3} >
+        <Avatar
+          alt="Remy Sharp"
+          src="/static/images/avatar/1.jpg"
+          sx={{ width: 250, height: 250 }}
+        >
+        </Avatar>
+      </Grid>
+      <Grid item xs={7}>
+        <Paper sx={{ boxShadow: "none" }}>
           닉네임: {memberInfo.nickname}<br />
           랭킹: {memberInfo.ranking}<br />
           점수: {memberInfo.score}<br />
           전적: {memberInfo.win}승 {memberInfo.lose}패<br />
         </Paper>
-        </Grid>
-        <Grid item xs={2} sx={{mt:-20}}>
-        <Button onClick={updateProfile}>프로필 수정</Button>
-        </Grid>
       </Grid>
+      <Grid item xs={2} sx={{ mt: -20 }}>
+        <Button onClick={updateProfile}>프로필 수정</Button>
+      </Grid>
+    </Grid>
   );
 }
