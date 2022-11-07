bcd68f15c3af8aad7211847aedcc9fa7062a0070
intae <kit938639@gmail.com>
2022-11-07 17:14:22 +0900

    :lemon: modify WebSocketException - 빠진 파일 업로드

---
+++ BE/the-knight/src/main/java/com/a301/theknight/global/error/exception/CustomRestException.java
@@ -0,0 +1,16 @@
+package com.a301.theknight.global.error.exception;
+
+import com.a301.theknight.global.error.errorcode.ErrorCode;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+public class CustomRestException extends RuntimeException implements CustomException {
+    private final ErrorCode errorCode;
+
+    public CustomRestException(ErrorCode errorCode) {
+        log.info("<<< [Error Occur] : Error name = {}, message = {}", errorCode.name(), errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
