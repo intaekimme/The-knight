495285aba0553eb158202e0a579c132f3b5937ab
intae <kit938639@gmail.com>
2022-11-07 13:25:35 +0900

    :bug: fix DummyDataTest 수정 - 원인 : lazy loading으로 인한 오류 - 해결 : fetch join 쿼리 작성

--- BE/the-knight/src/main/java/com/a301/theknight/domain/game/repository/GameRepository.java
+++ BE/the-knight/src/main/java/com/a301/theknight/domain/game/repository/GameRepository.java
@@ -8,12 +8,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
     @EntityGraph(attributePaths = {"players"})
     Page<Game> findByTitleIsContaining(String keyword, Pageable pageable);
 
+    @Query("select distinct g from Game g left join fetch g.players")
+    List<Game> findAllByFetchJoin();
+
+    @Query("select g from Game g join fetch g.players where g.id = :id")
+    Optional<Game> findByIdFetchJoin(@Param("id") long gameId);
     @Query("SELECT g FROM Game g " +
             "JOIN FETCH Player " +
             "WHERE g.id = :gameId ")
