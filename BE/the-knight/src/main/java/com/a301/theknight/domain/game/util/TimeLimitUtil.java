2492e4ce17d8fc05c0c9b5f53279f2cb33489da6
김현교 <zkdlajt96@naver.com>
2022-11-07 23:11:54 +0900

    :memo: docs : 코드 Save를 위한 commit

--- BE/the-knight/src/main/java/com/a301/theknight/domain/game/util/TimeLimitUtil.java
+++ BE/the-knight/src/main/java/com/a301/theknight/domain/game/util/TimeLimitUtil.java
@@ -45,13 +45,17 @@ public class TimeLimitUtil {
                 return;
             }
 
+            //convert 때문에 얘를 저장하는데, 서버에 /force-convert 를 따로 만들면 될 듯
+            //거기는 nextStatus를 인자로 받아서 그걸 바로 프론트에 리턴해주는 방식
             curInGame.changeStatus(nextStatus);
             redisRepository.saveInGame(gameId, curInGame);
+
             RLock lock = redissonClient.getLock(timeLockKeyGen(gameId));
             lock.tryLock(1, 1, TimeUnit.SECONDS);
             // /convert 요청
             sendMessageService.convertCall(gameId);
-            //TODO: [강제 로직 수행~~] -> curStatus에 맞게 로직을 수행해야 함.
+            //TODO: [강제 로직 수행~~] -> curStatus에 매핑된 force logic을 수행
+            // Message를 보내주어 수행? -> 해당 메소드가 끝나고 timeLock을 해제해주어야 함.
             // 각 Status별 강제 수행 로직 정리하기.
             // 로직 수행하면서 Status를 nextStatus로 바꿔주는 방식도 좋을듯??
 
