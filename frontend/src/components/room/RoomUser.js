022842f8af4b17ad4704ac29c4ce68b656f21757
박민호 <tkdfhralsgh@naver.com>
2022-11-08 17:56:52 +0900

    modify: 방입장 websocket 수정, 대기방 전체 유저저장 완료

--- frontend/src/components/room/RoomUser.js
+++ frontend/src/components/room/RoomUser.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Grid, Box } from "@mui/material";
 import UserBox from "../../commons/user/UserBox";
 
 export default function RoomUser(props) {
-	// console.log(props.userDatas.slice(0, parseInt(props.maxUser / 2)));
-	// console.log(props.userDatas.slice(parseInt(props.maxUser / 2), props.userDatas.length));
+	const emptyUser = {
+		nickname: "",
+		image: "",
+		ranking: -1,
+		score: -1,
+		win: -1,
+		lose: -1,
+	};
+	const [userDatas, setUserDatas] = React.useState([]);
+	React.useEffect(()=>{
+		const tempDatas = [...props.userDatas];
+		while(tempDatas.length < props.maxUser){
+			tempDatas.push(emptyUser);
+		}
+		console.log(tempDatas);
+		setUserDatas(tempDatas);
+	}, [props.userDatas]);
 	return (
 		<Box sx={{p: 3, display: "flex", justifyContent: "space-evenly",flexDirection: "column", width: "70%", height: "100%"}}>
 			<Box sx={{display: "flex", justifyContent: "space-evenly"}}>
-				{props.userDatas.slice(0, parseInt(props.maxUser / 2)).map((userData, index) => (
+				{userDatas.slice(0, parseInt(props.maxUser / 2)).map((userData, index) => (
 					<UserBox key={`user${userData.nickname}${index + parseInt(props.maxUser / 2) - parseInt(props.maxUser / 2)}`}
 						userData={userData} width={props.size * 6} height={props.size * 6}/>
 				))}
 			</Box>
 			<Box sx={{ display: "flex", justifyContent: "space-evenly"}}>
-				{props.userDatas.slice(parseInt(props.maxUser / 2), props.userDatas.length).map((userData, index) => (
+				{userDatas.slice(parseInt(props.maxUser / 2), userDatas.length).map((userData, index) => (
 					<UserBox key={`user${userData.nickname}${index + parseInt(props.maxUser / 2)}`} 
 					userData={userData} width={props.size*6} height={props.size*6}/>
 				))}
