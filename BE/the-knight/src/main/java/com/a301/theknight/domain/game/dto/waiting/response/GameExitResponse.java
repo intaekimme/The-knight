a298aac9cc99b4580555ba8fd1848723e84fcd9c
InTae <kit938639@gmail.com>
2022-11-09 20:39:11 +0900

    :bug: fix GameExitResponse - @Data 어노테이션 붙음

--- BE/the-knight/src/main/java/com/a301/theknight/domain/game/dto/waiting/response/GameExitResponse.java
+++ BE/the-knight/src/main/java/com/a301/theknight/domain/game/dto/waiting/response/GameExitResponse.java
@@ -1,8 +1,10 @@
 package com.a301.theknight.domain.game.dto.waiting.response;
 
 import lombok.Builder;
+import lombok.Data;
 
 
+@Data
 @Builder
 public class GameExitResponse {
     private boolean exit;
