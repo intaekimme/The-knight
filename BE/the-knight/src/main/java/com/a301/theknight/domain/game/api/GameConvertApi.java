fbd348363495b2a8b99bdfd33846fce7c967f539
김현교 <zkdlajt96@naver.com>
2022-11-11 15:18:25 +0900

    :bug: fix : Server To Server 요청 제거
    - 서버 -> 서버로 웹소켓 보내는 요청 모두 제거.
    - GameConvertUtil 클래스로 convert관련 로직 모두 이동.

--- BE/the-knight/src/main/java/com/a301/theknight/domain/game/api/GameConvertApi.java
+++ BE/the-knight/src/main/java/com/a301/theknight/domain/game/api/GameConvertApi.java
@@ -1,47 +1,27 @@
 package com.a301.theknight.domain.game.api;
 
 import com.a301.theknight.domain.common.service.SendMessageService;
-import com.a301.theknight.domain.game.dto.convert.GameStatusResponse;
-import com.a301.theknight.domain.game.service.GameConvertService;
+import com.a301.theknight.domain.game.dto.convert.PostfixDto;
+import com.a301.theknight.domain.game.util.GameConvertUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 import javax.validation.constraints.Min;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class GameConvertApi {
 
     private final SendMessageService sendMessageService;
-    private final GameConvertService gameConvertService;
-
-    @MessageMapping(value = "/games/{gameId}/convert")
-    public void publishConvert(@Min(1) @DestinationVariable long gameId) {
-        GameStatusResponse gameStatusResponse = gameConvertService.getGameStatus(gameId);
-        sendMessageService.sendData(gameId,"/convert",gameStatusResponse);
-    }
-
-    @MessageMapping(value = "/games/{gameId}/force-convert")
-    public void forceConvert(@DestinationVariable long gameId) {
-        GameStatusResponse gameStatusResponse = gameConvertService.getNextGameStatus(gameId);
-        sendMessageService.sendData(gameId,"/convert",gameStatusResponse);
-    }
+    private final GameConvertUtil gameConvertUtil;
 
     @MessageMapping(value = "/games/{gameId}/convert-complete")
     public void convertComplete(@Min(1) @DestinationVariable long gameId) {
-        List<String> postfixList = gameConvertService.convertComplete(gameId);
-        if (postfixList != null) {
-            postfixList.forEach(postfix -> sendMessageService.sendDataToServer(gameId, postfix));
+        PostfixDto postfixDto = gameConvertUtil.completeConvertPrepare(gameId);
+        if (postfixDto != null) {
+            sendMessageService.sendData(gameId, "/complete", postfixDto);
         }
     }
-
-    @MessageMapping(value = "/games/{gameId}/proceed")
-    public void proceedGame(@Min(1) @DestinationVariable long gameId) {
-        GameStatusResponse gameStatus = gameConvertService.getGameStatus(gameId);
-        sendMessageService.sendData(gameId, "/proceed", gameStatus);
-    }
-
 }
