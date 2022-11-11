ede51939e59eca21cdd819b4aaeb36fbf1341554
intae <kit938639@gmail.com>
2022-11-11 17:03:54 +0900

    :lemon: modify ready response QA 반영 - ReadyDto 수정

--- BE/the-knight/src/test/java/com/a301/theknight/domain/game/service/GameServiceTest.java
+++ BE/the-knight/src/test/java/com/a301/theknight/domain/game/service/GameServiceTest.java
@@ -1,15 +1,20 @@
 package com.a301.theknight.domain.game.service;
 
+import com.a301.theknight.domain.game.dto.waiting.response.GameInfoResponse;
+import com.a301.theknight.domain.game.entity.Game;
+import com.a301.theknight.domain.game.repository.GameRepository;
 import com.a301.theknight.domain.member.entity.Member;
 import com.a301.theknight.domain.member.repository.MemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import com.a301.theknight.domain.player.entity.Player;
+import com.a301.theknight.domain.player.repository.PlayerRepository;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
 import static org.junit.jupiter.api.Assertions.*;
 @Disabled
 @ExtendWith(MockitoExtension.class)
@@ -17,33 +22,59 @@ class GameServiceTest {
 
     @Mock
     MemberRepository memberRepository;
-
     @Mock
+    GameRepository gameRepository;
+    @Mock
+    PlayerRepository playerRepository;
+
     GameService gameService;
+
+
+    private Member[] testMembers;
+    private Game testGame;
 
     @BeforeEach
     void setUp() {
-        // 유저 생성
-        for(int i=1; i<=10; i++){
-            Member member = Member.builder()
-                    .email("player" + Integer.toString(i) + "@email.com")
-                    .password("player" + Integer.toString(i))
-                    .nickname("player" + Integer.toString(i))
-                    .image("image" + Integer.toString(i))
-                    .build();
+        testMembers = new Member[4];
 
-            memberRepository.save(member);
+        for(int i=0; i<4; i++){
+            testMembers[i] = new Member((long)(i),
+                    "testPlayer" + i + "@email.com",
+                    "testPlayer" + i,
+                    "testPlayer" + i,
+                    "testImage" + i,
+                    "ROLE_USER");
         }
 
-        //  게임방 생성
-//        gameService.createGame();
+        testGame = new Game(1L, "testGame", 4,3,2,1,10 );
+        Player owner = new Player(1L, testMembers[0], testGame);
+        owner.setOwner();
+
+        gameService = new GameService(gameRepository,memberRepository,playerRepository);
     }
 
-    @AfterEach
-    void tearDown() {
+    @DisplayName("게임방 생성")
+    @Test
+    void createGame() {
+        // given
+        // when
+        // then
     }
+
 
     @Test
     void getGameList() {
+    }
+
+    @DisplayName("게임 상세 정보 조회")
+    @Test
+    void testMethodNameHere() {
+        // given
+        given(memberRepository.findById(1L)).willReturn(Optional.of(testMembers[0]));
+        given(gameRepository.findById(1L)).willReturn(Optional.of(testGame));
+        // when
+        GameInfoResponse gameInfoResponse = gameService.getGameInfo(1L, 1L);
+        // then
+        assertEquals(1L, gameInfoResponse.getOwnerId());
     }
 }
