c6f5b1136f15a07d598c4daa57f511268aa5ba30
이현진 <hjnee1102@gmail.com>
2022-11-11 17:00:57 +0900

    :sparkles: feat : GameReceivers 작성ing
    - 약 80% 진행

--- frontend/src/_slice/gameSlice.js
+++ frontend/src/_slice/gameSlice.js
@@ -7,7 +7,12 @@ const meInit = {
   index: 4,
   team: "B",
   pass: false,
-}
+};
+
+const timerInit = {
+  timer: 0,
+  intervalObject: null,
+};
 
 const playersInit = {
   state: "",
@@ -42,7 +47,7 @@ const playersInit = {
     },
     {
       memberId: 4,
-      nickname: 'Bob',
+      nickname: "Bob",
       leftCount: 0,
       rightCount: 0,
       team: "B",
@@ -51,7 +56,7 @@ const playersInit = {
     },
     {
       memberId: 5,
-      nickname: 'Tom',
+      nickname: "Tom",
       leftCount: 0,
       rightCount: 0,
       team: "B",
@@ -60,120 +65,228 @@ const playersInit = {
     },
     {
       memberId: 6,
-      nickname: 'Ria',
+      nickname: "Ria",
       leftCount: 0,
       rightCount: 0,
       team: "B",
       order: 0,
       weapons: [null, null],
     },
-  ]
+  ],
+};
+
+const leaderInit = {
+  A: null,
+  B: null
 }
+
+const attackFirstInit = ""
 
 const orderInit = {
   A: [null, null, null, null, null],
   B: [null, null, null, null, null],
+};
+
+const countWeaponInit = {
+  A: {},
+  B: {},
+}
+
+const isSelectCompleteInit = {
+  A: false,
+  B: false,
 }
 
 // PREPARE, PREDECESSOR, ATTACK, ATTACK_DOUBT, DEFENSE, DEFENSE_DOUBT, DOUBT_RESULT, EXECUTE(공&방 결과), END
-const phaseInit = "EXECUTE"
-const previousPhaseInit = null
+const phaseInit = "EXECUTE";
+const previousPhaseInit = null;
 
-const isLoadingInit = false
+const isLoadingInit = false;
 
 const currentAttackerInit = {
   memberId: 1,
-  nickname: "John",
-  leftCount: 0,
-  rightCount: 0,
   team: "A",
-  order: 0,
-  weapons: [null, null],
-}
+};
 
 const currentDefenserInit = {
   memberId: 3,
-  nickname: "Sara",
-  leftCount: 0,
-  rightCount: 0,
   team: "A",
-  order: 0,
-  weapons: [null, null],
+};
+
+const attackInfoInit = {
+  attacker: {},
+  defenser: {},
+  weapon: "",
+  hand: "",
 }
+
+const defenseInfoInit = {
+  defenser: {},
+  weapon: "",
+  hand: "",
+}
+
+const endInfoInit = {
+  isWin: true,
+  losingteam: "B",
+  losingLeaderId: null,
+  winningLeaderId: null,
+  players: [],
+};
 
 export const gameSlice = createSlice({
   name: "gameSlice",
   initialState: {
     me: meInit,
+    timer: timerInit,
     players: playersInit,
+    leader: leaderInit,
+    attakFirst: attackFirstInit,
     order: orderInit,
+    countweapon: countWeaponInit,
+    isSelectComplete: isSelectCompleteInit,
     phase: phaseInit,
     previousPhase: previousPhaseInit,
     isLoading: isLoadingInit,
     currentAttacker: currentAttackerInit,
     currentDefenser: currentDefenserInit,
+    attackInfo: attackInfoInit,
+    defenseInfo: defenseInfoInit,
+    endInfo: endInfoInit,
   },
   reducers: {
-    fetchMe: (state) => {
-      const memberId = window.localStorage.getItem("memberId")
-      const player = state.players.players.find((player) => player.memberId === memberId)
-      const playerIndex = state.players.players.findIndex((player) => player.memberId === memberId)
-    
+    setMe: (state) => {
+      const memberId = window.localStorage.getItem("memberId");
+      const player = state.players.players.find((player) => player.memberId === memberId);
+      const playerIndex = state.players.players.findIndex((player) => player.memberId === memberId);
+
       state.me = {
         memberId: memberId,
         nickname: player.nickname,
         index: playerIndex,
         team: player.team,
+      };
+    },
+    setTimer: (state, action) => {
+      state.timer.timer = action.payload;
+    },
+    countTimer: (state, action) => {
+      state.timer.timer = state.timer.timer - 1;
+      state.timer.intervalObject = action.payload;
+    },
+    stopCountTimer: (state) => {
+      if (state.timer.timer <= 0) {
+        state.timer.timer = 0;
+        clearInterval(state.timer.intervalObject);
       }
     },
     fetchPlayers: (state, action) => {
-      state.players = action.payload
+      state.players = action.payload;
     },
     selectWeapon: (state, action) => {
       if (!state.players.players[state.me.index].weapons[0]) {
-        state.players.players[state.me.index].weapons[0] = action.payload
+        state.players.players[state.me.index].weapons[0] = action.payload;
       } else if (!state.players.players[state.me.index].weapons[1]) {
-        state.players.players[state.me.index].weapons[1] = action.payload
+        state.players.players[state.me.index].weapons[1] = action.payload;
       }
     },
     deleteWeapon: (state, action) => {
-      if (action.payload === 'left') {
-        state.players.players[state.me.index].weapons[0] = null
-      } else if (action.payload === 'right') {
-        state.players.players[state.me.index].weapons[1] = null
+      if (action.payload === "left") {
+        state.players.players[state.me.index].weapons[0] = null;
+      } else if (action.payload === "right") {
+        state.players.players[state.me.index].weapons[1] = null;
       }
     },
     selectOrder: (state, action) => {
       // 선택한 순서가 비어있고, 유저가 이미 선택한 순서가 없을 때
       if (!state.order[state.me.team][action.payload]) {
-        for (let i=0; i < state.order[state.me.team].length; i++) {
+        for (let i = 0; i < state.order[state.me.team].length; i++) {
           // 선택한 순서가 비어있고, (다른 순서를 이미 선택해뒀을 때)
           if (state.order[state.me.team][i]) {
-            if (state.order[state.me.team][i].memberId === state.players.players[state.me.index].memberId) {
-              state.order[state.me.team][i] = null
+            if (
+              state.order[state.me.team][i].memberId ===
+              state.players.players[state.me.index].memberId
+            ) {
+              state.order[state.me.team][i] = null;
             }
           }
         }
-        state.order[state.me.team][action.payload] = state.players.players[state.me.index]
+        state.order[state.me.team][action.payload] = state.players.players[state.me.index];
         // (선택한 순서가 차있고), 유저가 선택했던 순서일 때
-      } else if (state.order[state.me.team][action.payload].memberId === state.players.players[state.me.index].memberId) {
-        state.order[state.me.team][action.payload] = null
+      } else if (
+        state.order[state.me.team][action.payload].memberId ===
+        state.players.players[state.me.index].memberId
+      ) {
+        state.order[state.me.team][action.payload] = null;
       }
     },
+    fetchOrder: (state, action) => {
+      state.order[state.me.team] = action.payload;
+    },
     fetchPhase: (state, action) => {
-      state.previousPhase = state.phase
-      state.phase = action.payload
+      state.previousPhase = state.phase;
+      state.phase = action.payload;
     },
     switchIsLoading: (state) => {
-      state.isLoading = !state.isLoading
+      state.isLoading = !state.isLoading;
     },
     selectPass: (state) => {
-      state.me.pass = true
+      state.me.pass = true;
     },
     initializePass: (state) => {
-      state.me.pass = false
+      state.me.pass = false;
+    },
+    setEndInfo: (state, action) => {
+      state.endInfo = action.payload;
+    },
+    setLeader: (state, action) => {
+      state.leader[state.me.team] = action.payload;
+    },
+    setAttackFirst: (state, action) => {
+      state.attackFirst = action.payload;
+    },
+    fetchCountWeapon: (state, action) => {
+      state.countweapon[state.me.team] = action.payload;
+    },
+    selectComplete: (state) => {
+      state.isSelectComplete[state.me.team] = true;
+    },
+    fetchCurrentAttacker: (state, action) => {
+      state.currentAttacker = action.payload;
+    },
+    fetchCurrentDefenser: (state, action) => {
+      state.currentDefenser = action.payload;
+    },
+    fetchAttackInfo: (state, action) => {
+      state.attackInfo = action.payload;
+    },
+    fetchDefenseInfo: (state, action) => {
+      state.defenseInfo = action.payload;
     },
   },
 });
-export const { fetchMe, fetchPlayers, selectWeapon, deleteWeapon, selectOrder, fetchPhase, switchIsLoading, selectPass, initializePass } = gameSlice.actions;
+export const {
+  setMe,
+  setTimer,
+  countTimer,
+  stopCountTimer,
+  fetchPlayers,
+  selectWeapon,
+  deleteWeapon,
+  selectOrder,
+  fetchOrder,
+  fetchPhase,
+  switchIsLoading,
+  selectPass,
+  initializePass,
+  setEndInfo,
+  setLeader,
+  setAttackFirst,
+  fetchCountWeapon,
+  selectComplete,
+  fetchCurrentAttacker,
+  fetchCurrentDefenser,
+  fetchAttackInfo,
+  fetchDefenseInfo,
+} = gameSlice.actions;
 export default gameSlice.reducer;
--- frontend/src/websocket/GameReceivers.js
+++ frontend/src/websocket/GameReceivers.js
@@ -1,10 +1,26 @@
 import { useDispatch, useSelector } from "react-redux";
-import { fetchPlayers } from "../_slice/gameSlice";
+import {
+  setTimer,
+  countTimer,
+  stopCountTimer,
+  fetchPlayers,
+  fetchOrder,
+  fetchPhase,
+  setEndInfo,
+  setLeader,
+  setAttackFirst,
+  fetchCountWeapon,
+  selectComplete,
+  fetchCurrentAttacker,
+  fetchCurrentDefenser,
+  fetchAttackInfo,
+  fetchDefenseInfo,
+} from "../_slice/gameSlice";
 import api from "../api/api";
 
 const dispatch = useDispatch();
-const stompClient = useSelector(state => state.websocket.stompClient);
-const myTeam = useSelector(state => state.game.me).team
+const stompClient = useSelector((state) => state.websocket.stompClient);
+const myTeam = useSelector((state) => state.game.me).team;
 
 ////////////////////////////////////////////////
 //////////////// Common(Connect) ///////////////
@@ -27,140 +43,232 @@ const onSubPlayersInfo = (payload) => {
   //     },
   //   ]
   // }
-  const data = JSON.parse(payload.body)
-  dispatch(fetchPlayers(data))
-}
+  const data = JSON.parse(payload.body);
+  dispatch(fetchPlayers(data));
+};
 
 // 최초 화면전환 요청
 const onSubConvert = (payload) => {
   // {
   //   gameStatus : String,
   // }
-  const nextPhase = JSON.parse(payload.body).gameStatus;
+  const data = JSON.parse(payload.body);
+  const nextPhase = data.gameStatus;
   // 화면전환을 위한 구독 준비
   if (nextPhase === "PREPARE") {
     // for prepare
-    stompClient.subscribe(api.subLeader(gameId, myTeam), onSubLeader)
-    stompClient.subscribe(api.subCountWeapon(gameId, myTeam), onSubCountWeapon)
-    stompClient.subscribe(api.subOrder(gameId, myTeam), onSubOrder)
-    stompClient.subscribe(api.subSelectComplete(gameId, myTeam), onSubSelectComplete)
+    stompClient.subscribe(api.subLeader(gameId, myTeam), onSubLeader);
+    stompClient.subscribe(api.subCountWeapon(gameId, myTeam), onSubCountWeapon);
+    stompClient.subscribe(api.subOrder(gameId, myTeam), onSubOrder);
+    stompClient.subscribe(api.subSelectComplete(gameId, myTeam), onSubSelectComplete);
   } else if (nextPhase === "PREDECESSOR") {
-    stompClient.subscribe(api.subAttackFirst(gameId), onSubAttackFirst)
+    stompClient.subscribe(api.subAttackFirst(gameId), onSubAttackFirst);
   } else if (nextPhase === "ATTACK") {
-    stompClient.subscribe(api.subCurrentAttacker(gameId, myTeam), onSubCurrentAttacker)
+    stompClient.subscribe(api.subCurrentAttacker(gameId, myTeam), onSubCurrentAttacker);
   } else if (nextPhase === "ATTACK_DOUBT") {
-    stompClient.subscribe(api.subAttackInfo(gameId), onSubAttackInfo)
+    stompClient.subscribe(api.subAttackInfo(gameId), onSubAttackInfo);
   } else if (nextPhase === "DEFENSE") {
-
   } else if (nextPhase === "DEFENSE_DOUBT") {
-    stompClient.subscribe(api.subDefenseInfo(gameId), onSubDefenseInfo)
+    stompClient.subscribe(api.subDefenseInfo(gameId), onSubDefenseInfo);
   } else if (nextPhase === "DOUBT_RESULT") {
-    stompClient.subscribe(api.subDoubtInfo(gameId), onSubDoubtInfo)
+    stompClient.subscribe(api.subDoubtInfo(gameId), onSubDoubtInfo);
   } else if (nextPhase === "EXECUTE") {
-    stompClient.subscribe(api.subExecute(gameId), onSubExecute)
+    stompClient.subscribe(api.subExecute(gameId), onSubExecute);
   } else if (nextPhase === "END") {
+  }
 
-  } 
-  // 준비완료 publish
-}
+  // 준비완료 sub/pub
+  stompClient.subscribe(api.subConvertComplete(gameId), onSubConvertComplete);
+  stompClient.send(api.pubConvertComplete(gameId), {}, {});
+};
+
+// 다음 phase에 필요한 정보 요청
+const onSubConvertComplete = (payload) => {
+  // {
+  //   postfix: String,
+  // }
+  const data = JSON.parse(payload.body);
+  stompClient.send(api.pubPostfix(gameId, data.posfix), {}, {});
+};
 
 // 실제 화면전환
-const onSubNextPhase = () => {
-  // phase 변경
-}
+const onSubNextPhase = (payload) => {
+  // {
+  //   gameStatus : String,
+  // }
+  const data = JSON.parse(payload.body);
+  dispatch(fetchPhase(data.gameStatus));
+};
 
-// 제한시간 
-const onSubTimer = () => {
-  // Redux에 제한시간 저장
-  // 1초씩 차감
-}
+// 제한시간 (변경사항이 많을 것으로 예상)
+const onSubTimer = (payload) => {
+  // {
+  //   remainTime : int
+  // }
+  const data = JSON.parse(payload.body);
+  dispatch(setTimer(data.remainTime));
+
+  const intervalObject = setInterval(() => {
+    // 1초씩 감소
+    dispatch(countTimer(intervalObject));
+    // 0 이면 카운트 중단
+    dispatch(stopCountTimer());
+  }, 1000);
+};
 
 // 게임결과
-const onSubEnd = () => {
-
-}
+const onSubEnd = (payload) => {
+  // {
+  //   isWin : boolean,
+  //   losingTeam : String (A, B),
+  //   losingLeaderId : long,
+  //   winningLeaderId : long,
+  //   players : [
+  //     memberId : long,
+  //     leftWeapon :
+  //     rightWeapon :
+  //   ], ...
+  // }
+  const data = JSON.parse(payload.body);
+  dispatch(setEndInfo(data));
+};
 
 ////////////////////////////////////////////////
 /////////////////// PREPARE ////////////////////
 ////////////////////////////////////////////////
 
 // 팀 리더
-const onSubLeader = () => {
-  // Redux에 각 팀 리더 저장
-}
+const onSubLeader = (payload) => {
+  // {
+  //   memberId : long
+  // }
+  const data = JSON.parse(payload.body);
+  dispatch(setLeader(data.memberId));
+};
 
 // 팀 남은 무기 개수
-const onSubCountWeapon = () => {
-
-}
+const onSubCountWeapon = (payload) => {
+  // {
+  //   sword : int,
+  //   twin : int,
+  //   shield : int,
+  //   hand : int
+  // }
+  const data = JSON.parse(payload.body);
+  dispatch(fetchCountWeapon(data));
+};
 
 // 팀 현재까지 선택된 순서
-const onSubOrder = () => {
-
-}
+const onSubOrder = (payload) => {
+  // {
+  //   orderList : [
+  //       memberId : long,
+  //       nickname : String,
+  //       image: String
+  //   ]
+  // }
+  const data = JSON.parse(payload.body);
+  dispatch(fetchOrder(data.orderList));
+};
 
 // 팀 무기&순서 선택완료
-const onSubSelectComplete = () => {
-
-}
+const onSubSelectComplete = (payload) => {
+  // {
+  //   selectCompleted: boolean
+  // }
+  const data = JSON.parse(payload.body);
+  if (data.selectCompleted) {
+    dispatch(selectComplete());
+  }
+};
 
 ////////////////////////////////////////////////
 ///////////////// PREDECESSOR //////////////////
 ////////////////////////////////////////////////
 
 // 선공 팀
-const onSubAttackFirst = () => {
-  // Redux에 선공 팀 저장
-}
+const onSubAttackFirst = (payload) => {
+  // {
+  //   preAttackTeam : String (A, B),
+  // }
+  const data = JSON.parse(payload.body);
+  dispatch(setAttackFirst(data.preAttackTeam));
+};
 
 ////////////////////////////////////////////////
 /////////////////// ATTACK /////////////////////
 ////////////////////////////////////////////////
 
 // 현재 공격자
-const onSubCurrentAttacker = () => {
-  // Redux에 공격자 저장
-}
+const onSubCurrentAttacker = (payload) => {
+  // {
+  //   memberId : long,
+  //   team: String,
+  // }
+  const data = JSON.parse(payload.body);
+  dispatch(fetchCurrentAttacker(data))
+};
+
+// 공격패스
 
 ////////////////////////////////////////////////
 ///////////////// Attack Doubt /////////////////
 ////////////////////////////////////////////////
 
 // 공격정보
-const onSubAttackInfo = () => {
-  // 공격정보 저장해서 doubt에서 띄워주기
-  // 현재 방어자 갱신
-}
+const onSubAttackInfo = (payload) => {
+  // {
+  //   attacker : {
+  //     memberId: long : 해당 유저가 공격을 진행할 것이다,
+  //     team: String
+  //   }
+  //   defenser : {
+  //     memberId: long : 공격 대상자(=다음 방어자),
+  //     team: String
+  //   }
+  //   weapon : String : 사용 선언무기
+  //    (SWORD, TWIN, SHEILD, HAND, HIDE)
+  //   hand : String (LEFT, RIGHT)
+  // }
+  const data = JSON.parse(payload.body);
+  dispatch(fetchCurrentDefenser(data.defenser))
+  dispatch(fetchAttackInfo(data))
+};
 
 ////////////////////////////////////////////////
 /////////////////// Defense ////////////////////
 ////////////////////////////////////////////////
 
+// 방어패스
 
 ////////////////////////////////////////////////
 ///////////////// Defense Doubt ////////////////
 ////////////////////////////////////////////////
 
 // 방어정보
-const onSubDefenseInfo = () => {
-  // 방어정보 갱신해서 doubt에 띄우기
-}
-
+const onSubDefenseInfo = (payload) => {
+  // {
+  //   defenser : {
+  //     memberId: long : 방어자,
+  //   }
+  //   weapon : String : 사용 선언무기
+  //     (SWORD, TWIN, SHEILD, HAND, HIDE)
+  //   hand : String (LEFT, RIGHT)
+  // }
+  const data = JSON.parse(payload.body);
+  dispatch(fetchDefenseInfo(data));
+};
 
 ////////////////////////////////////////////////
 ///////////////// Doubt Result /////////////////
 ////////////////////////////////////////////////
 
 // 의심정보
-const onSubDoubtInfo = () => {
-
-}
+const onSubDoubtInfo = () => {};
 
 ////////////////////////////////////////////////
 /////////////////// Execute ////////////////////
 ////////////////////////////////////////////////
 
 // 애니메이션 수행 정보
-const onSubExecute = () => {
-
-}
+const onSubExecute = () => {};
