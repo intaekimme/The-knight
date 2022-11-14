824a4a0992f81d749f9cdbff9e789c1bc3647e29
김현교 <zkdlajt96@naver.com>
2022-11-14 10:24:48 +0900

    :white_check_mark: test : InGamePlayer 단위테스트 추가
    - InGamePlayer 저장, 수정 단위테스트 추가.

--- BE/the-knight/src/test/java/com/a301/theknight/domain/game/repository/GameRedisRepositoryTest.java
+++ BE/the-knight/src/test/java/com/a301/theknight/domain/game/repository/GameRedisRepositoryTest.java
@@ -1,14 +1,15 @@
 package com.a301.theknight.domain.game.repository;
 
 import com.a301.theknight.domain.game.entity.GameStatus;
-import com.a301.theknight.domain.game.entity.redis.InGame;
-import com.a301.theknight.domain.game.entity.redis.TeamInfoData;
-import com.a301.theknight.domain.game.entity.redis.TurnData;
+import com.a301.theknight.domain.game.entity.redis.*;
+import com.a301.theknight.domain.player.entity.Team;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,7 +21,10 @@ class GameRedisRepositoryTest {
     GameRedisRepository redisRepository;
 
     long gameId = 1L;
+    long memberId = 10L;
     InGame inGame;
+    InGamePlayer inGamePlayer;
+    GameWeaponData gameWeaponData;
 
     @BeforeEach
     void setup() {
@@ -29,6 +33,19 @@ class GameRedisRepositoryTest {
                 .teamAInfo(TeamInfoData.builder().build())
                 .teamBInfo(TeamInfoData.builder().build()).build();
         redisRepository.saveInGame(gameId, inGame);
+
+        gameWeaponData = GameWeaponData.builder()
+                .twin(2)
+                .sword(2)
+                .shield(2)
+                .hand(2).build();
+        redisRepository.saveGameWeaponData(gameId, Team.A, gameWeaponData);
+
+        inGamePlayer = InGamePlayer.builder()
+                .memberId(memberId)
+                .nickname("nickname")
+                .build();
+        redisRepository.saveInGamePlayer(gameId, memberId, inGamePlayer);
     }
 
     @Test
@@ -40,7 +57,7 @@ class GameRedisRepositoryTest {
     }
 
     @Test
-    @DisplayName("InGame 변경 테스트")
+    @DisplayName("InGame 수정 테스트")
     void change() {
         InGame changeInGame = InGame.builder()
                 .gameStatus(GameStatus.ATTACK)
@@ -51,5 +68,27 @@ class GameRedisRepositoryTest {
         InGame returnInGame = redisRepository.getInGame(gameId).get();
 
         assertThat(returnInGame.getGameStatus()).isEqualTo(changeInGame.getGameStatus());
+    }
+
+    @Test
+    @DisplayName("InGamePlayer 저장 테스트")
+    void savePlayer() {
+        InGamePlayer returnPlayer = redisRepository.getInGamePlayer(gameId, memberId).get();
+
+        assertThat(returnPlayer.getNickname()).isEqualTo(inGamePlayer.getNickname());
+    }
+
+    @Test
+    @DisplayName("InGamePlayer 수정 테스트")
+    void changeInGamePlayer() {
+        InGamePlayer changePlayer = InGamePlayer.builder()
+                .memberId(memberId)
+                .nickname("change nickname")
+                .build();
+        redisRepository.saveInGamePlayer(gameId, memberId, changePlayer);
+
+        InGamePlayer returnPlayer = redisRepository.getInGamePlayer(gameId, memberId).get();
+
+        assertThat(returnPlayer.getNickname()).isEqualTo(changePlayer.getNickname());
     }
 }
