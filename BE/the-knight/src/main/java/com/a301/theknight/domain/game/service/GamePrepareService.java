6e9dcf73d8e9a6e594cfafeef146465d6cce2366
김현교 <zkdlajt96@naver.com>
2022-11-16 08:44:28 +0900

    :lemon: modify : 무기, 순서 선택 후 선공화면에서 /players 보내주기

--- BE/the-knight/src/main/java/com/a301/theknight/domain/game/service/GamePrepareService.java
+++ BE/the-knight/src/main/java/com/a301/theknight/domain/game/service/GamePrepareService.java
@@ -35,9 +35,9 @@ public class GamePrepareService {
     private final GameRedisRepository redisRepository;
 
     @Transactional
-    public GamePlayersInfoResponse getPlayersInfo(long gameId) {
-        GamePlayersInfoDto playersAInfo = getPlayersInfo(gameId, Team.A);
-        GamePlayersInfoDto playersBInfo = getPlayersInfo(gameId, Team.B);
+    public GamePlayersInfoResponse getTeamPlayersInfo(long gameId) {
+        GamePlayersInfoDto playersAInfo = getTeamPlayersInfo(gameId, Team.A);
+        GamePlayersInfoDto playersBInfo = getTeamPlayersInfo(gameId, Team.B);
 
         return GamePlayersInfoResponse.builder()
                 .playersAInfoDto(playersAInfo)
@@ -128,7 +128,7 @@ public class GamePrepareService {
         return new SelectCompleteDto(inGame.isAllSelected(), team);
     }
 
-    private GamePlayersInfoDto getPlayersInfo(long gameId, Team team) {
+    private GamePlayersInfoDto getTeamPlayersInfo(long gameId, Team team) {
         List<InGamePlayer> playerList = redisRepository.getInGamePlayerList(gameId);
 
         List<PlayerDataDto> players = playerList.stream()
