ae620fe6ef3c7e76b31e858ebb4b34d4b5b5f9ec
김현교 <zkdlajt96@naver.com>
2022-11-16 15:55:02 +0900

    :lemon: modify : TimeLimit change status 로직 수정

--- BE/the-knight/src/main/java/com/a301/theknight/domain/game/util/GameConvertUtil.java
+++ BE/the-knight/src/main/java/com/a301/theknight/domain/game/util/GameConvertUtil.java
@@ -36,19 +36,20 @@ public class GameConvertUtil {
 
     @Transactional
     public ConvertResponse convertScreen(long gameId) {
-        InGame inGame = initCountValue(gameId);
-
+        InGame inGame = getInGame(gameId);
         GameStatus gameStatus = inGame.getGameStatus();
+
+        initCountValue(gameId, inGame);
         return new ConvertResponse(gameStatus.name());
     }
 
     @Transactional
     public ConvertResponse forceConvertScreen(long gameId) {
-        InGame inGame = initCountValue(gameId);
-
+        InGame inGame = getInGame(gameId);
         GameStatus curStatus = inGame.getGameStatus();
         GameStatus nextStatus = getNextStatus(gameId, inGame, curStatus);
 
+        initCountValue(gameId, inGame, nextStatus);
         return new ConvertResponse(nextStatus.name());
     }
 
@@ -71,15 +72,28 @@ public class GameConvertUtil {
         }
     }
 
-    private InGame initCountValue(long gameId) {
+    private void initCountValue(long gameId, InGame inGame) {
         RLock countLock = redissonClient.getLock(generateCountKey(gameId));
         try {
             tryAcquireCountLock(countLock);
 
-            InGame inGame = getInGame(gameId);
             inGame.initRequestCount();
             gameRedisRepository.saveInGame(gameId, inGame);
-            return inGame;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            unLock(countLock);
+        }
+    }
+
+    private void initCountValue(long gameId, InGame inGame, GameStatus nextStatus) {
+        RLock countLock = redissonClient.getLock(generateCountKey(gameId));
+        try {
+            tryAcquireCountLock(countLock);
+
+            inGame.initRequestCount();
+            inGame.changeStatus(nextStatus);
+            gameRedisRepository.saveInGame(gameId, inGame);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
--- BE/the-knight/src/main/java/com/a301/theknight/domain/limit/service/DefaultTimeLimitService.java
+++ BE/the-knight/src/main/java/com/a301/theknight/domain/limit/service/DefaultTimeLimitService.java
@@ -1,32 +1,21 @@
 package com.a301.theknight.domain.limit.service;
 
-import com.a301.theknight.domain.game.entity.GameStatus;
 import com.a301.theknight.domain.game.entity.redis.InGame;
 import com.a301.theknight.domain.game.repository.GameRedisRepository;
 import com.a301.theknight.domain.game.util.GameConvertUtil;
 import com.a301.theknight.domain.limit.template.TimeLimitServiceTemplate;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DefaultTimeLimitService extends TimeLimitServiceTemplate {
 
-    private final GameRedisRepository redisRepository;
-    private final GameConvertUtil gameConvertUtil;
 
     public DefaultTimeLimitService(GameRedisRepository redisRepository, RedissonClient redissonClient, GameConvertUtil gameConvertUtil) {
         super(redisRepository, redissonClient);
-        this.redisRepository = redisRepository;
-        this.gameConvertUtil = gameConvertUtil;
     }
 
     @Override
-    @Transactional
     public void runLimitLogic(long gameId, InGame inGame) {
-        GameStatus nextStatus = gameConvertUtil.getNextStatus(gameId, inGame, inGame.getGameStatus());
-
-        inGame.changeStatus(nextStatus);
-        redisRepository.saveInGame(gameId, inGame);
     }
 }
