555206beceffdd968d7dce6a0662abfdab455448
박민호 <tkdfhralsgh@naver.com>
2022-11-18 10:23:28 +0900

    modify: Room CSS 파일분리(js파일)

--- frontend/src/_css/RoomUserCSSProperties.js
+++ frontend/src/_css/RoomUserCSSProperties.js
@@ -1,12 +1,6 @@
 import React from "react";
-import { white } from "./ReactCSSProperties";
 
-const leftInfoGrid = (size) => {return {fontSize:size, display: "flex", alignItems: "center"};};
-const settingIcon = (size) => {return { color:"gray", fontSize: size*2 };};
-const rightInfoGrid = (size) => {return { fontSize: size*1.2, display: "flex", justifyContent: "center", alignItems: "center" };};
-const selectTeamBox = (size) => {return {width:size*10, height: size*4, border: "7px solid #4d4d4d", borderRadius: "10px", display: "flex", alignItems: "center", justifyContent: "center"};};
-const selectTeamButton = (size) => {return { margin:0.5, color:white, fontSize: size, minWidth: size*2, width: size*2, height: size*2, borderRadius: "50%" };};
-const exitGrid = (size) => {return {display: "flex", justifyContent: "center", alignItems: "center"};};
-const exitIcon = (size) => {return { color:"red", fontSize: size*2 };};
+const RoomUserBox = (size) => {return {p: 3, display: "flex", justifyContent: "space-evenly",flexDirection: "column", width: "100%", height: "100%"};};
+const RoomUserListBox = (size) => {return { display: "flex", justifyContent: "space-evenly" };};
 
-export { leftInfoGrid, settingIcon, rightInfoGrid, selectTeamBox, selectTeamButton, exitGrid, exitIcon };
+export { RoomUserBox, RoomUserListBox };
