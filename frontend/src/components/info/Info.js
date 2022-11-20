3fe24cf5f71e3409d0778b37694e56f3baa42e50
이현빈 <amy7852@gmail.com>
2022-11-21 08:07:55 +0900

    :lipstick: style : 소개 페이지 영상 삽입

--- frontend/src/components/info/Info.js
+++ frontend/src/components/info/Info.js
@@ -1,7 +1,8 @@
 import React from "react";
 import infoLogo from '../../_assets/infoLogo.png'
 import { Box } from "@mui/system";
-import { Avatar, Container, Typography } from "@mui/material";
+import { Avatar, Container, Grid, Typography } from "@mui/material";
+import video from '../../_assets/info/video.mp4'
 
 
 export default function Info() {
@@ -9,9 +10,19 @@ export default function Info() {
         <Container fixed sx={{ width: 1000 }}>
             <Box sx={{
                 pt: 10,
+                pb: 10,
             }}>
-                <Box sx={{ bgcolor: '#fff', opacity: 0.7, height: 1000, display: 'flex', justifyContent: 'center', boxShadow: '2px 2px 2px 10px #fff' }}>
-                    <Avatar src={infoLogo} sx={{ pt: 5, width: 'auto', height: 150 }} variant="square"></Avatar>
+                <Box sx={{ bgcolor: '#fff', opacity: 0.7, height: 800, display: 'flex', justifyContent: 'center', boxShadow: '2px 2px 2px 10px #fff' }}>
+                    <Grid container direction="column" alignItems={'center'} spacing={6}>
+                    <Grid item>
+                    <Avatar src={infoLogo} sx={{ pt: 5, width: 500, height: 'auto' }} variant="square"></Avatar>
+                    </Grid>
+                    <Grid item>
+                    <video controls width="850" style={{opacity: 1}}>
+                        <source src={video} type="video/mp4"></source>
+                    </video>
+                    </Grid>
+                    </Grid>
                 </Box>
             </Box>
         </Container>
