5cc0694b09d19ed26a8c3fac5b3c9fdbc19c020f
LeeHyunJin1997 <hjnee1102s@gmail.com>
2022-11-23 23:17:53 +0900

    :bug: fix : 멤버페이지 로그인 리다이렉팅 활성화

--- frontend/src/pages/MemberPage.js
+++ frontend/src/pages/MemberPage.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import LoginCheck from "../commons/login/LoginCheck";
+
 import MemberInfoForm from '../components/memberPage/MemberInfoForm'
 import CurrentRecord from '../components/memberPage/CurrentRecord'
 import UpdateMemInfo from '../components/memberPage/UpdateMemInfo';
 
-// import { useNavigate } from 'react-router-dom';
-// import LoginCheck from "../commons/login/LoginCheck";
 import "../_css/Mypage.module.css"
 import { Container } from "@mui/system";
 import { Grid } from "@mui/material";
 import styled from "../_css/Mypage.module.css";
 
 export default function MemberPage() {
-  // const isLogin = LoginCheck();
-  // const navigate = useNavigate();
-  // React.useEffect(()=>{
-  //   if(!isLogin){
-  //     navigate('/login');
-  //   }
-  // }, []);
+
+  const isLogin = LoginCheck();
+  const navigate = useNavigate();
+  React.useEffect(()=>{
+    if(!isLogin){
+      navigate('/login');
+    }
+  }, []);
+
   const [clickUpdate, setClickUpdate] = useState(false);
   const updateProfile = () => {
     console.log("this");
