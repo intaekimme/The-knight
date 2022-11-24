41523220ea9507b4bc966854ff2faf91251657f8
LeeHyunJin1997 <hjnee1102s@gmail.com>
2022-11-24 21:26:30 +0900

    :sparkles: feat : 최근 전적 레이아웃 변경
    - 아이콘, 이름 중앙정렬
    - 아이콘 크기 반응형

--- frontend/src/components/memberPage/OppositeMem.js
+++ frontend/src/components/memberPage/OppositeMem.js
@@ -3,22 +3,36 @@ import React from "react";
 import { Avatar, Grid } from "@mui/material";
 
 export default function OppositeMem(props) {
-  const oppoMems = props.oppoMems
+  const oppoMems = props.oppoMems;
 
   return (
-    <Grid container direction="row" alignItems="center">
+    <Grid
+      container
+      direction="row"
+      alignItems="center"
+    >
       {oppoMems.map((oppoMem, key) => {
         return (
-          <Grid item key={key} sx={{ pt: 2, pb: 2, pl: 1, pr: 1 }}>
-            <Grid sx={{ pl: '15%' }}>
-              <Avatar src={oppoMem.image} sx={{ height: 50, width: 50 }}></Avatar>
-            </Grid>
+          <Grid
+            item
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              width: "7.5vmin",
+            }}
+            key={key}
+          >
             <Grid>
-              {oppoMem.nickname}
+              <Avatar
+                src={oppoMem.image}
+                sx={{ height: "4.5vmin", width: "4.5vmin" }}
+              ></Avatar>
             </Grid>
+            <Grid>{oppoMem.nickname}</Grid>
           </Grid>
-        )
+        );
       })}
     </Grid>
-  )
+  );
 }
